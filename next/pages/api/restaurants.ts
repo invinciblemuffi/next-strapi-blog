0b19659bf4218e98e8171de1d40fb2d4ec2b82f2
vijay-fs <vijaykarthik9868@gmail.com>
2022-05-10 18:23:33 +0530

    env added

--- next/pages/api/restaurants.ts
+++ next/pages/api/restaurants.ts
@@ -1,4 +1,5 @@
 export async function fetchRestaurants(path: string) {
+  console.log("api url",process.env.NEXT_PUBLIC_API_URL)
   try {
     const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api${path}`, {
       method: 'GET',
@@ -6,6 +7,7 @@ export async function fetchRestaurants(path: string) {
         'Content-Type': 'application/json',
       },
     });
+    console.log("checking url",resp.headers)
     const respJSON = await resp.json();
     return respJSON.data
   } catch (error) {
--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,8 +1,8 @@
-module.exports = ({ env }) => ({
+module.exports = () => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: "ADMIN_JWT_SECRET",
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "API_TOKEN_SALT",
   },
 });
